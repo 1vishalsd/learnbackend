d3f5d970c3967fe102a44e4206c3ec0b0a4c8e0c
1VishalSD <ilearnvk@gmail.com>
2024-09-18 16:57:26 +0530

    setup s connection with database

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+
+import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    application.on("error", (error) => {
+      console.log("ERRR :", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port : ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log("ERROR :", error);
+  }
+})();
