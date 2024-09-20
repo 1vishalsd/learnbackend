ef4dea819c1be525efb2ab0e1626cf07573e81bd
1VishalSD <ilearnvk@gmail.com>
2024-09-20 06:53:15 +0530

    make the apiError

---
+++ src/utils/apiError.js
@@ -0,0 +1,22 @@
+class apiError extends Error {
+  constructor(
+    statusCode,
+    massage = "Something went wrong",
+    errors = [],
+    statck = ""
+  ) {
+    super(massage);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.massage = massage;
+    this.success = false;
+    this.errors = errors;
+
+    if (statck) {
+      this.stack = statck;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+export { apiError };
